095c49813b90e2dee315adc0abecca05f65804ad
Nikolina Djogova <nikolina.djogova@stud.unilu.ch>
2026-03-24 21:43:37 +0100

    Loading packages and initial options.

--- scripts/00_setup.R
+++ scripts/00_setup.R
@@ -1,1 +1,18 @@
+# Loading packages I might need 
+
+library(httr)
+library(jsonlite)
 library(tidyverse)
+library(stringr)
+library(lubridate)
+installed.packages("here")
+library(here)
+
+#Setting reproducible paths
+data_raw_path <- here("data", "raw")
+data_interim_path <- here("data", "interim")
+data_processed_path <- here("data", "processed")
+
+# Setting options
+options(stringsAsFactors = FALSE)
+
