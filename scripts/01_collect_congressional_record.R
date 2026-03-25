edb218db435b6ce0104aa3b0092b1f60641dbe19
Nikolina Djogova <nikolina.djogova@stud.unilu.ch>
2026-03-25 11:25:59 +0100

    API key loaded succesfully.

--- scripts/01_collect_congressional_record.R
+++ scripts/01_collect_congressional_record.R
@@ -4,3 +4,11 @@ rm(list = ls())
 # Loading project setup 
 source(here::here("scripts", "00_setup.R"))
 
+# Acquired the API key, credentials are being kept in the R environment.
+
+api_key <- Sys.getenv("GOVINFO_API_KEY")
+if (api_key == "") {
+  stop("API key is missing from the R environment")
+} else {
+  message("It works! API key successfully retrieved.")
+}
