932f3c6ec403362a83f8b1f8f05ceba23c810e52
Nikolina Djogova <nikolina.djogova@stud.unilu.ch>
2026-03-26 09:44:43 +0100

    Build function to get text from one granule.

--- scripts/02_collect_house_granules_from_package.R
+++ scripts/02_collect_house_granules_from_package.R
@@ -2,10 +2,28 @@
 rm(list = ls())
 
 # Loading project setup
-source(here("scripts", "00_setup.R"))
+source(here::here("scripts", "00_setup.R"))
 
 # Getting the API key 
 api_key <- Sys.getenv("GOVINFO_API_KEY")
 if (api_key == "") {
   stop("The key is missing from the environment")
 }
+
+# Testing one Congressional Record package 
+test_package <- "CREC-2026-02-11"
+
+# Function to get text from one granule
+get_granule_text <- function(granule_link, api_key) {
+  granule_url <- paste0(granule_link, "?api_key=", api_key)
+  granule_response <- httr::GET(granule_url)
+  granulate_json <- jsonlite::fromJSON(
+    httr::content(granule_response, as = "text", encoding = "UTF-8")
+    )
+  clean_text <- granulate_text |>
+    xml2::read_html() |>
+    rvest::html_element("pre") |>
+    rvest::html_text()
+  return(clean_text)
+}
+
