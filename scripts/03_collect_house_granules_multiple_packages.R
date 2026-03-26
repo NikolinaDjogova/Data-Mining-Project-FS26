d22ea40fca99e2bc47da02aa886be7f96b1603be
Nikolina Djogova <nikolina.djogova@stud.unilu.ch>
2026-03-26 22:03:18 +0100

    Function to get text from one granule.

--- scripts/03_collect_house_granules_multiple_packages.R
+++ scripts/03_collect_house_granules_multiple_packages.R
@@ -10,3 +10,32 @@ if (api_key == "") {
   stop("The key is missing from the environment")
 }
 
+# Testing small set of Congressional Record packages
+test_packages <- c(
+"CREC-2026-02-11", 
+"CREC-2026-02-12",
+"CREC-2026-02-13"
+)
+
+# Function to get text from one granule
+get_granule_text <- function(granule_link, api_key) {
+  granule_url <- paste0(granule_link, "?api_key=", api_key)
+  granule_response <- httr::GET(granule_url)
+  
+  granule_json <- jsonlite::fromJSON(
+    httr::content(granule_response, as = "text", encoding = "UTF-8")
+  )
+  
+  text_url <- paste0(granule_json$download$txtLink, "?api_key=", api_key)
+  text_response <- httr::GET(text_url)
+  
+  raw_text <- httr::content(text_response, as = "text", encoding = "UTF-8")
+  
+  clean_text <- raw_text |>
+    xml2::read_html() |>
+    rvest::html_element("pre") |>
+    rvest::html_text()
+  
+  return(clean_text)
+}
+
