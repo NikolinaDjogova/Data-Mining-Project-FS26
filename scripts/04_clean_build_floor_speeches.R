96486691a7de2e386ca200bf93a4dbe04a974e46
Nikolina Djogova <nikolina.djogova@stud.unilu.ch>
2026-03-27 09:26:41 +0100

    Create a list of procedural titles to be excluded.

--- scripts/04_clean_build_floor_speeches.R
+++ scripts/04_clean_build_floor_speeches.R
@@ -10,10 +10,29 @@ if (api_key == "") {
   stop("The key is missing from the environment")
 }
 
-# small set of packages 
+# Small set of packages 
 package_ids <- c(
   "CREC-2026-02-11", 
   "CREC-2026-02-12",
   "CREC-2026-02-13"
 )
+
+# Creating a list of precedural titles 
+excluded_titles <- c(
+  "House of Representatives",
+  "PRAYER",
+  "THE JOURNAL",
+  "PLEDGE OF ALLEGIANCE",
+  "ADJOURNMENT",
+  "RECESS",
+  "AFTER RECESS",
+  "MESSAGES FROM THE PRESIDENT",
+  "EXECUTIVE COMMUNICATIONS, ETC.",
+  "REPORTS OF COMMITTEES ON PUBLIC BILLS AND RESOLUTIONS",
+  "PUBLIC BILLS AND RESOLUTIONS",
+  "ADDITIONAL SPONSORS",
+  "DISCHARGE PETITIONS",
+  "DISCHARGE PETITIONS-- ADDITIONS AND WITHDRAWALS"
+)
+  ##These are House granules that are not likely to contain substantive speeches for my project 
 
