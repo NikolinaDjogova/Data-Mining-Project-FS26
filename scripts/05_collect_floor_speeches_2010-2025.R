a36e2460534cc42db6d9a55a9a9ea74cc89439bc
Nikolina Djogova <nikolina.djogova@stud.unilu.ch>
2026-03-27 10:17:29 +0100

    Set up new script.

---
+++ scripts/05_collect_floor_speeches_2010-2025.R
@@ -0,0 +1,13 @@
+# Starting with a clean environment 
+rm(list = ls())
+
+# Loading project setup 
+source(here::here("scripts", "00_setup.R"))
+source(here::here("scripts", "functions_congressional_record.R"))
+
+# Getting the API key
+api_key <- Sys.getenv("GOVINFO_API_KEY")
+if (api_key == "") {
+  stop("The key is missing from the environment")
+}
+
