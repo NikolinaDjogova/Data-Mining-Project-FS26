c102ccd4054f1803b33b10be9e0c8e9998143f75
Nikolina Djogova <nikolina.djogova@stud.unilu.ch>
2026-03-27 10:32:00 +0100

    Build wrappers and update llm disclosure note.

--- scripts/05_collect_floor_speeches_2010-2025.R
+++ scripts/05_collect_floor_speeches_2010-2025.R
@@ -3,7 +3,7 @@ rm(list = ls())
 
 # Loading project setup 
 source(here::here("scripts", "00_setup.R"))
-source(here::here("scripts", "functions_congressional_record.R"))
+source(here::here("scripts", "reusables.R"))
 
 # Getting the API key
 api_key <- Sys.getenv("GOVINFO_API_KEY")
@@ -20,3 +20,25 @@ all_dates <- seq.Date(
 
 # Building package IDs for the full date range
 package_ids <- paste0("CREC-", all_dates)
+
+# Wrapper for packages 
+get_house_granules_wrapper <- function(package_id, api_key) {
+  tryCatch(
+    get_house_granules(package_id, api_key),
+    error = function(e) {
+      message(paste("Error in package:", package_id, " - ", e$message))
+      return("tibble::tibble()")
+    }
+  )
+}
+
+# Wrapper for granules 
+get_granule_text_wrapper <- function(granule_link, api_key) {
+  tryCatch(
+    get_granule_text(granule_link, api_key),
+    error = function(e) {
+      message(paste("Error in granule link:", granule_link, " - ", e$message))
+      return(NA_character_)
+    }
+  )
+}
