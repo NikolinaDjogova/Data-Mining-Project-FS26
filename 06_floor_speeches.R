51318ac70b0e8f5d2e764016c4ea31dc722bc9b8
Nikolina Djogova <nikolina.djogova@stud.unilu.ch>
2026-04-10 14:19:34 +0200

    Get chunk files

--- 06_floor_speeches.R
+++ 06_floor_speeches.R
@@ -5,3 +5,11 @@ source(here::here("scripts", "00_setup.R"))
 if (!dir.exists(interim_floor_speeches_path)) {
   stop("Interim floor speeches folder does not exist")
 }
+
+# Get all chunk files
+chunk_files <- list.files(
+  interim_floor_speeches_path,
+  pattern = "^floor_speeches_chunk_[0-9]+\\.csv$",
+  full.names = TRUE
+)
+
