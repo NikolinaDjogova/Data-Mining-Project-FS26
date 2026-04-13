a81c54c26b2175f02420fd75fe6fd3316ae20607
Nikolina Djogova <nikolina.djogova@stud.unilu.ch>
2026-04-13 08:51:09 +0200

    Edit reusables

--- scripts/Reusables.R
+++ scripts/Reusables.R
@@ -69,3 +69,53 @@ get_granule_text_wrapper <- function(granule_link, api_key) {
     }
   )
 }
+
+# Wrapper for packages 
+get_house_granules_wrapper <- function(package_id, api_key) {
+  tryCatch(
+    get_house_granules(package_id, api_key),
+    error = function(e) {
+      message(paste("Error in package:", package_id, " - ", e$message))
+      return(tibble::tibble())
+    }
+  )
+}
+
+# Wrapper for granules 
+get_granule_text_wrapper <- function(granule_link, api_key) {
+  tryCatch(
+    get_granule_text(granule_link, api_key),
+    error = function(e) {
+      message(paste("Error in granule link:", granule_link, " - ", e$message))
+      return(NA_character_)
+    }
+  )
+}
+
+get_granule_text <- function(granule_link, api_key) {
+  
+  granule_url <- paste0(granule_link, "?api_key=", api_key)
+  
+  granule_json <- jsonlite::fromJSON(
+    httr::content(httr::GET(granule_url), as = "text", encoding = "UTF-8")
+  )
+  
+  text_url <- paste0(granule_json$download$txtLink, "?api_key=", api_key)
+  
+  raw_text <- httr::content(
+    httr::GET(text_url),
+    as = "text",
+    encoding = "UTF-8"
+  )
+  
+  html <- xml2::read_html(raw_text)
+  pre_node <- rvest::html_element(html, "pre")
+  
+  clean_text <- if (!is.na(pre_node) && length(pre_node) > 0) {
+    rvest::html_text(pre_node)
+  } else {
+    rvest::html_text(html)
+  }
+  
+  stringr::str_squish(clean_text)
+}
