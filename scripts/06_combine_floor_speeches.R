5df1a385e5ec4725ddb15ebcb3108e1bf4459d56
Nikolina Djogova <nikolina.djogova@stud.unilu.ch>
2026-04-13 09:57:04 +0200

    Sort files for consistency

--- scripts/06_combine_floor_speeches.R
+++ scripts/06_combine_floor_speeches.R
@@ -19,3 +19,18 @@ all_chunk_files <- list.files(
 if (length(all_chunk_files) == 0) {
   stop("No chunk files were found")
 }
+
+# Sorting files for readability and consistency
+chunk_names <- basename(all_chunk_files)
+
+chunk_type <- ifelse(
+  stringr::str_detect(chunk_names, "recent"),
+  "recent",
+  "original"
+)
+
+chunk_number <- stringr::str_extract(chunk_names, "[0-9]+") |>
+  as.integer()
+
+all_chunk_files <- all_chunk_files[order(chunk_type, chunk_number)]
+
