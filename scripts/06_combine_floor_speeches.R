49f71c32364fdab5f1251944d9fc069aa9c0d1cc
Nikolina Djogova <nikolina.djogova@stud.unilu.ch>
2026-04-13 09:47:33 +0200

    Interim folder check

--- scripts/06_combine_floor_speeches.R
+++ scripts/06_combine_floor_speeches.R
@@ -3,3 +3,7 @@ rm(list = ls())
 source(here::here("scripts", "00_setup.R"))
 source(here::here("scripts", "reusables.R"))
 
+# Making sure the interim folder exists
+if (!dir.exists(interim_floor_speeches_path)) {
+  stop("Interim floor speeches folder does not exist")
+}
