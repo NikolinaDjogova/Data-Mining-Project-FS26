f0e458cac12067150bc0eed0704f2444d45ab868
Nikolina Djogova <nikolina.djogova@stud.unilu.ch>
2026-04-13 09:51:40 +0200

    Combining all files

--- scripts/06_combine_floor_speeches.R
+++ scripts/06_combine_floor_speeches.R
@@ -7,3 +7,15 @@ source(here::here("scripts", "reusables.R"))
 if (!dir.exists(interim_floor_speeches_path)) {
   stop("Interim floor speeches folder does not exist")
 }
+
+# Getting all chunk files
+all_chunk_files <- list.files(
+  interim_floor_speeches_path,
+  pattern = "^floor_speeches(_recent)?_chunk_[0-9]+\\.csv$",
+  full.names = TRUE
+)
+
+# Stop if no chunk files are found
+if (length(all_chunk_files) == 0) {
+  stop("No chunk files were found")
+}
