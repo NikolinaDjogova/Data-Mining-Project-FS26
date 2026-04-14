0bc5d7c0c55e9936e63e81dfa8a6fdf86312f25d
Nikolina Djogova <nikolina.djogova@stud.unilu.ch>
2026-04-14 11:15:22 +0200

    Final edit

--- scripts/02_collect_house_granules_from_package.R
+++ scripts/02_collect_house_granules_from_package.R
@@ -3,6 +3,7 @@ rm(list = ls())
 
 # Loading project setup
 source(here::here("scripts", "00_setup.R"))
+source(here::here("scripts", "reusables.R"))
 
 # Getting the API key 
 api_key <- Sys.getenv("GOVINFO_API_KEY")
@@ -12,23 +13,6 @@ if (api_key == "") {
 
 # Testing one Congressional Record package 
 test_package <- "CREC-2026-02-11"
-
-# Function to get text from one granule
-get_granule_text <- function(granule_link, api_key) {
-  granule_url <- paste0(granule_link, "?api_key=", api_key)
-  granule_response <- httr::GET(granule_url)
-  granule_json <- jsonlite::fromJSON(
-    httr::content(granule_response, as = "text", encoding = "UTF-8")
-    )
-  text_url <- paste0(granule_json$download$txtLink, "?api_key=", api_key)
-  text_response <- httr::GET(text_url)
-  raw_text <-   httr::content(text_response, as = "text", encoding = "UTF-8")
-  clean_text <- raw_text |>
-    xml2::read_html() |>
-    rvest::html_element("pre") |>
-    rvest::html_text()
-  return(clean_text)
-}
 
 # Requesting package summary 
 base_url <- "https://api.govinfo.gov/packages"
